e428f8364594d6a2462b3d5f6df9decadb737579
r-harini <harini.ramesh17@gmail.com>
2019-07-15 19:33:48 +0530

    DSA lab

--- DSA_lab/Experiment1_1.c
+++ DSA_lab/Experiment1_1.c
@@ -1,26 +1,23 @@
 #include<stdio.h>
 
 void main(){
-int a[10],n,h=0;
+int a[10],n,h=0,x;
 printf("Enter n:");
 scanf("%d",&n);
 for (int i=0;i<n;i++){
     scanf("%d",&a[i]);
 }
 if (n%2==0){
-    for (int i=0;i<(n/2)+1;i++){
-        if (a[i]!=a[n-i-1]){
-            h++;
-        }
-    }
+    x=(n/2)+1;
 }
 else{
-    for (int i=0;i<(n/2);i++){
+    x=n/2;
+}
+for (int i=0;i<x;i++){
         if (a[i]!=a[n-i-1]){
             h++;
         }
     }
-}
 if (h!=0){
     printf ("Not same");
 }
