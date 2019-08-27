17a9a044030f18d359a6881e8f8badd624a7ae1f
r-harini <harini.ramesh17@gmail.com>
2019-08-27 22:53:45 +0530

    DSA project

---
+++ Crawler/Spider.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from general import *
+
+class Spider:
+
+    #Class variables(shared among all instances)
+    project_name=''
+    base_url=''
+    domain_name=''
+    queue_file=''
+    crawled_file=''
+    queue=set()
+    crawled=set()
+
+    def __init__(self, project_name,base_url, domain_name):
+        Spider.project_name=project_name
+        Spider.base_url=base_url
+        Spider.domain_name=domain_name
+        Spider.queue_file=Spider.project_name+'/queue.txt'
+        Spider.crawled_file=Spider.project_name+'/crawled.txt'
+        self.boot()
+        self.crawl_page('First Spider', Spider.base_url)
